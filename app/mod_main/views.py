ee8ce75e2812ca8684cafcd4f21b3f3ed72f332c
Your Name <medinaoshlani6@gmail.com>
2017-02-05 14:49:33 +0000

    Files are added.

--- app/mod_main/views.py
+++ app/mod_main/views.py
@@ -1,7 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint,render_template
+from app import mongo
 
 mod_main = Blueprint('main', __name__)
 
 @mod_main.route('/')
 def index():
-	return "Flask Hello World"
+	db = mongo.db.arkep
+	db.insert({"name": "arkep"})
+	return render_template("index.html")
+
+@mod_main.route('/form')
+def form():
+	namee="Techstitution"
+	return render_template("form.html", name=namee)
